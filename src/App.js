bc573a1aa127923519d66a072723cf1dfd20ed3c
DabboussiAmal <amaldabboussi74@gmail.com>
2022-03-21 14:27:21 +0100

    Props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Profile from './profile/Profile';
+import image from './Images/MyPhoto.jpg'
 function App() {
+
+  let handleClick= ()=>{
+    alert('hiii')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className="App"
+     style={{display: 'flex' ,border:'solid 1px transparent' , width:'800px', marginRight:'auto',marginTop:'30px',marginLeft:'auto',background: 'rgb(34,193,195)',
+     background: 'linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)' }}>
+      <Profile  fullName="Dabboussi Amal"  age="21"  bio="Etudiante en 3éme année à ISLAIB, spécialisté génie logiciels et système d'informations" handleClick= {handleClick}>
+        <img src={image} alt="Photo"style={{width:'300px', height:'300px', marginLeft:'auto', marginRight:'auto' ,marginTop:'10px',marginBottom:'10px'}} />
+      </Profile>
     </div>
   );
 }
---
+++ src/profile/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Profile = ({fullName ,age ,bio, children, handleClick}) => {
+  return (
+    <div style={{display:'flex' ,flexDirection:'column' , marginLeft:'auto',marginRight:'auto'}}>
+        <h1 style={{textSize:'70',color:'#094679' , fontFamily:'Cursive,serif'}}> Hi, it's me !</h1>
+        <span style={{textSize:'100px',color:'#094679' , fontFamily:'Cursive,serif'}}>  {fullName}</span>
+        <span style={{textSize:'100px',color:'#094679' , fontFamily:'Cursive,serif'}}>  {age}ans </span>
+        <span style={{textSize:'100px',color:'#094679' , fontFamily:'Cursive,serif'}}> {bio}</span>
+        {children }
+        <button style={{width:'100px',height:'40px', backgroundColor:'#094679',marginLeft:'300px'}} onClick={handleClick}>Click Me !!> </button>
+    </div>
+  )
+}
+
+export default Profile
